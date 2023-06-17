5faec2375c7ef2730db41d5f1d132a6c050a5329
qualwebs <adam@qualwebs.com>
2023-06-17 20:21:42 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
 const {readdirSync} = require('fs')
-const app = express()
+const index = express()
 
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+index.use(express.json())
+index.use(cors())
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync('./routes').map((route) => index.use('/', require('./routes/' + route)))
 
 const server = () => {
     db()
-    app.listen(PORT, () => {
+    index.listen(PORT, () => {
         console.log('listening to port:', PORT)
     })
 }
