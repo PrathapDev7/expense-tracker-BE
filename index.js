f79bfab6a9d581a35d82eca002399e30689dc86b
qualwebs_1111 <adam@qualwebs.com>
2024-10-20 11:54:29 +0530

    Cron job

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ index.use(cors());
 readdirSync('./routes').map((route) => index.use('/api/v1/', require('./routes/' + route)));
 
 // Cron job to keep the API server active by pinging the server itself every minute
-cron.schedule('* 6-23 * * *', async () => {
+cron.schedule('* * * * *', async () => {
     try {
         const serverUrl = `https://expense-tracker-be-3rvm.onrender.com/api/v1/`;  // Make sure this is an actual route
         await axios.get(serverUrl);
